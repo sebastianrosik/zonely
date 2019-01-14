95df53d5c5d92c258dd30068b1365d135c146e23
Sebastian Rosik <sebastian.rosik@gmail.com>
2019-01-14 18:49:11 +0100

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Zonely
+
+World time zone converter application written in React.
+
+## Installation
+
+```
+$ npm install
+```
+
+## Development
+
+```
+$ npm run develop
+```
+
+or:
+
+```
+$ npm start
+```
+
+This will run development server on http://localhost:3000
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};
