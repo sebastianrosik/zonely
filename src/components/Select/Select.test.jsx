eb75c98cbe27fd9976877fdc7f3eeccc4c535e93
Sebastian Rosik <sebastian.rosik@gmail.com>
2019-01-23 22:56:59 +0100

    chore: Fixed unit test for last bug fix

--- src/components/Select/Select.test.jsx
+++ src/components/Select/Select.test.jsx
@@ -5,6 +5,8 @@ import { EUROPE_WARSAW, AMERICA_NEWYORK } from '../../fakes/timeZones';
 
 const fakeEvent = { preventDefault: jest.fn() };
 const fakeItems = [EUROPE_WARSAW, AMERICA_NEWYORK];
+const KEY_DOWN = 40;
+const KEY_UP = 38;
 
 describe('Select', () => {
   let wrapper;
@@ -58,7 +60,11 @@ describe('Select', () => {
     wrapper.setProps({
       onClose: onCloseMock
     });
-    wrapper.instance().shortcutsHandler('CLOSE');
+    const fakeEvent = {
+      preventDefault: jest.fn(),
+      keyCode: KEY_DOWN
+    };
+    wrapper.instance().shortcutsHandler('CLOSE', fakeEvent);
     expect(onCloseMock).toHaveBeenCalled();
   });
   it('calls onSelect when a SELECT action is passed through shortcut manager', () => {
@@ -70,7 +76,32 @@ describe('Select', () => {
     wrapper.setState({
       value: mockedValue
     });
-    wrapper.instance().shortcutsHandler('SELECT');
+    const fakeEvent = {
+      preventDefault: jest.fn(),
+      keyCode: KEY_DOWN
+    };
+    wrapper.instance().shortcutsHandler('SELECT', fakeEvent);
     expect(onSelectMock).toHaveBeenCalledWith(mockedValue);
+  });
+
+  it('prevents default event when key down or key up is pressed', () => {
+    expect.assertions(2);
+    [KEY_DOWN, KEY_UP].forEach(keyCode => {
+      const fakeEvent = {
+        preventDefault: jest.fn(),
+        keyCode
+      };
+      wrapper.instance().shortcutsHandler('SELECT', fakeEvent);
+      expect(fakeEvent.preventDefault).toHaveBeenCalled();
+    });
+  });
+
+  it('does not prevent default event when other keys than key down or key up are pressed', () => {
+    const fakeEvent = {
+      preventDefault: jest.fn(),
+      keyCode: 0
+    };
+    wrapper.instance().shortcutsHandler('SELECT', fakeEvent);
+    expect(fakeEvent.preventDefault).not.toHaveBeenCalled();
   });
 });
